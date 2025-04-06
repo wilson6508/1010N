a4389c00e279c239ef9712e4fa95478849d33e4d
wilson6508 <wilson650837@gmail.com>
2025-04-06 22:46:36 +0800

    update

---
+++ 1_Java/1_Course/016_入門/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello-World_5566");
+    }
+}
+
+// javac HelloWorld.java
+// java HelloWorld
+---------------------------------------------------------------------------------------------
+原本的
+Path = C:\Program Files\Common Files\Oracle\Java\javapath
+
+新的
+JAVA_HOME = C:\Program Files\Java\jdk-17.0.5
+Path = %JAVA_HOME%\bin
+---------------------------------------------------------------------------------------------
